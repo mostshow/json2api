2163948a1bd8c02e93119ef67f6dccdeeffbaf30
lyndon <mostshow@163.com>
2017-11-26 16:02:50 +0800

    add bodyparser

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import 'babel-register'
 
 import Koa from 'koa'
 import cors from'koa-cors'
+import bodyParser from'koa-bodyparser'
 import router from './server/router'
 
 const app = new Koa();
 // app.use(cors())
+app.use(bodyParser())
 app.use(router.routes())
 .use(router.allowedMethods())
 
